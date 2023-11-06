0d0da6269b1b9dad43bcd0955dc2c1f488aa865f
Fadza <fadza0077@gmail.com>
2023-11-07 02:24:03 +0700

    feat: add detail movie

---
+++ app/src/main/java/com/example/submissiondicodingpemula/DetailMovieActivity.kt
@@ -0,0 +1,16 @@
+package com.example.submissiondicodingpemula
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.submissiondicodingpemula.databinding.ActivityDetailMovieBinding
+
+class DetailMovieActivity : AppCompatActivity() {
+
+    private  lateinit var binding: ActivityDetailMovieBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailMovieBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
